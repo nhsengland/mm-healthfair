6648814d34798a83e306ce476c8b588df5c76536
sophmrtn <44570734+sophmrtn@users.noreply.github.com>
2024-04-25 22:43:25 +0100

    Adds filters based on min/max stays and events

--- src/extract_episodes_from_subjects.py
+++ src/extract_episodes_from_subjects.py
@@ -9,7 +9,7 @@ from data.preprocessing import (
 from data.subject import (
     add_hours_elapsed_to_events,
     convert_events_to_timeseries,
-    get_events_for_stay,
+    get_events_in_period,
     read_events,
     read_stays,
 )
@@ -73,74 +73,71 @@ for subject_dir in tqdm(
     episodic_data = assemble_episodic_data(stays)
 
     if episodic_data.shape[0] == 0:
-        sys.stderr.write(
-            f"Failed to get stay data for: {subject_id}. May cause issues\n"
-        )
+        sys.stderr.write(f"Warning: Failed to get stay data for: {subject_id}.\n")
 
     # clean events
     events = clean_events(events)
 
     if events.shape[0] == 0:
         # no valid events for this subject
+        print(f"No events found for subject {subject_id}")
         continue
 
     timeseries = convert_events_to_timeseries(events)
 
-    # extracting separate episodes (by hospital admission since using labevents)
-    for i in range(stays.shape[0]):
-        # for ed data
-        stay_id = stays.stay_id.iloc[i]
-        intime = stays.intime.iloc[i]
-        outtime = stays.outtime.iloc[i]
+    # extracting separate episodes
 
-        episode = get_events_for_stay(
-            timeseries, stay_id, intime, outtime
-        )  # get events during this ed stay
+    # counter for stays that are processed, cleaned and successfully written to disk
+    n = 0
 
-        if episode.shape[0] < args.min_events or episode.shape[0] < args.max_events:
+    for stay_idx in range(stays.shape[0]):
+        stay_id = stays.stay_id.iloc[stay_idx]
+        intime = stays.intime.iloc[stay_idx]
+        outtime = stays.outtime.iloc[stay_idx]
+        hadm_id = stays.hadm_id.iloc[stay_idx]
+        admittime = stays.admittime.iloc[stay_idx]
+        dischtime = stays.dischtime.iloc[stay_idx]
+
+        # get ed events during this specific ed/hosp stay (within certain window of time (optional))
+        episode = get_events_in_period(timeseries, stay_id, hadm_id, intime, dischtime)
+
+        if episode.shape[0] < args.min_events or episode.shape[0] > args.max_events:
             # if no data for this episode (or less than min or more than max) then continue
             # only keep stays with nb of datapoints within specific range
+            filter_by_nb_events += 1
             continue
 
+        # set index of episode as the time elapsed since ED intime
         episode = (
             add_hours_elapsed_to_events(episode, intime)
             .set_index("hours")
             .sort_index(axis=0)
         )
 
+        # note some stay_id = -1 but this file is just for linking to static variables and can be linked via hadm_id instead
         episodic_data.loc[episodic_data.index == stay_id].to_csv(
-            os.path.join(args.subjects_root_path, subject_dir, f"episode{i+1}.csv"),
+            os.path.join(args.subjects_root_path, subject_dir, f"episode{n+1}.csv"),
             index_label="stay_id",
         )
-
-        # for hosp data
-        # hadm_id = stays.hadm_id.iloc[i]
-        # admittime = stays.admittime.iloc[i]
-        # dischtime = stays.dischtime.iloc[i]
-
-        # episode = get_events_for_admission(timeseries, hadm_id, admittime, dischtime)
-
-        # if episode.shape[0] == 0:
-        #     # no data for this episode
-        #     continue
-
-        # episode = add_hours_elapsed_to_events(episode, admittime).set_index('hours').sort_index(axis=0)
-
-        # episodic_data.loc[episodic_data.index == hadm_id].to_csv(os.path.join(args.subjects_root_path, subject_dir,
-        #                                                                       f'episode{i+1}.csv'),
-        #                                                          index_label='stay')
 
         columns_str = [str(x) for x in list(episode.columns)]
         columns_str = map(lambda x: "" if x == "hours" else x, columns_str)
         sorted_indices = [
             i[0] for i in sorted(enumerate(columns_str), key=lambda x: x[1])
         ]
-        # columns_sorted = sorted(columns_str, key=(lambda x: "" if x == "hours" else x))
 
         episode = episode[[episode.columns[i] for i in sorted_indices]]
         episode.to_csv(
             os.path.join(
-                args.subjects_root_path, subject_dir, f"episode{i+1}_timeseries.csv"
+                args.subjects_root_path, subject_dir, f"episode{n+1}_timeseries.csv"
             ),
             index_label="hours",
         )
+
+        # add to counter once data has been written to disk
+        n += 1
+
+
+print(
+    f"SKIPPING {filter_by_nb_events} EVENTS, AND {filter_by_nb_stays} STAYS, FAILED TO READ {failed_to_read} SUBJECTS"
+)
