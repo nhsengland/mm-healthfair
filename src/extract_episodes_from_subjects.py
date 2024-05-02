a312fc4ea728b422a7258c1b7579a2a0df157618
sophmrtn <44570734+sophmrtn@users.noreply.github.com>
2024-05-02 13:37:30 +0100

    Add imputation strategies to timeseries preprocessing

--- src/extract_episodes_from_subjects.py
+++ src/extract_episodes_from_subjects.py
@@ -1,24 +1,28 @@
 import argparse
+import glob
 import os
 import sys
 
-from data.preprocessing import (
+from tqdm import tqdm
+from utils.functions import dataframe_from_csv, get_pop_means
+from utils.preprocessing import (
     assemble_episodic_data,
     clean_events,
 )
-from data.subject import (
+from utils.subject import (
     add_hours_elapsed_to_events,
     convert_events_to_timeseries,
     get_events_in_period,
     read_events,
     read_stays,
 )
-from data.util import dataframe_from_csv
-from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description="Extract episodes from per-subject data.")
 parser.add_argument(
     "subjects_root_path", type=str, help="Directory containing subject sub-directories."
+)
+parser.add_argument(
+    "mimic4_path", type=str, help="Directory containing MIMIC-IV HOSP CSV files."
 )
 parser.add_argument(
     "--min_stays", type=int, default=1, help="Minimum number of stays per subject."
@@ -32,7 +36,13 @@ parser.add_argument(
 parser.add_argument(
     "--subject_list",
     type=str,
-    help="File containing list of subject folders to include.",
+    help="File containing list of subjects to include.",
+)
+parser.add_argument(
+    "--impute_strategy",
+    "-i",
+    type=str,
+    help="Which strategy to use to impute missing values in timeseries data.",
 )
 parser.add_argument("--verbose", "-v", action="store_true", help="Verbosity.")
 
@@ -50,10 +60,26 @@ if args.verbose:
         f"EXTRACTING EPISODES FROM {len(os.listdir(args.subjects_root_path))} subjects..."
     )
 
+# If episodes exist then remove and start over
+if len(glob.glob(os.path.join(args.subjects_root_path, "*", "episode*"))) > 0:
+    response = input("Will need to overwrite existing episodes... continue? (y/n)")
+    if response == "y":
+        for f in glob.glob(os.path.join(args.subjects_root_path, "*", "episode*")):
+            try:
+                os.remove(f)
+            except OSError as ex:
+                print(ex)
+                sys.exit()
+    else:
+        print("Exiting..")
+        sys.exit()
+
 failed_to_read = 0
 filter_by_nb_stays = 0
 filter_by_nb_events = 0
 completed_subjects = 0
+
+# mean_values = get_pop_means(args.subjects_root_path)
 
 for subject_dir in tqdm(subject_list, desc="Iterating over subjects"):
     dn = os.path.join(args.subjects_root_path, subject_dir)
@@ -69,7 +95,9 @@ for subject_dir in tqdm(subject_list, desc="Iterating over subjects"):
         # reading tables of this subject
         stays = read_stays(os.path.join(args.subjects_root_path, subject_dir))
         # diagnoses = read_diagnoses(os.path.join(args.subjects_root_path, subject_dir))
-        events = read_events(os.path.join(args.subjects_root_path, subject_dir))
+        events = read_events(
+            os.path.join(args.subjects_root_path, subject_dir), args.mimic4_path
+        )
     except Exception:
         if args.verbose:
             sys.stderr.write(
@@ -89,6 +117,7 @@ for subject_dir in tqdm(subject_list, desc="Iterating over subjects"):
         sys.stderr.write(f"Warning: Failed to get stay data for: {subject_id}.\n")
 
     # clean events
+    # TODO: Check these functions work as expected - currently only filling "__" values and converting to floats, but may want to do outlier detection here, or any unit conversion
     events = clean_events(events)
 
     if events.shape[0] == 0:
@@ -98,6 +127,8 @@ for subject_dir in tqdm(subject_list, desc="Iterating over subjects"):
         continue
 
     timeseries = convert_events_to_timeseries(events)
+    min_events = 1 if args.min_events is None else args.min_events
+    max_events = 1e6 if args.max_events is None else args.max_events
 
     # extracting separate episodes per stay
     n = 0
@@ -112,10 +143,31 @@ for subject_dir in tqdm(subject_list, desc="Iterating over subjects"):
         # get ed events during this specific ed/hosp stay (within certain window of time (optional))
         episode = get_events_in_period(timeseries, stay_id, hadm_id, intime, dischtime)
 
-        min_events = 1 if args.min_events is None else args.min_events
-        max_events = 1e6 if args.max_events is None else args.max_events
+        # Aggregate into time-windows e.g., every 30mins from 00:00:00 to 23:59:59 (so all data is in same intervals)
+        episode = episode.resample("2h", on="charttime").mean().reset_index()
 
-        if episode.shape[0] < args.min_events or episode.shape[0] > args.max_events:
+        # Imputating of missing values using masking (adds features) or filling
+        if args.impute_strategy is not None:
+            if args.impute_strategy == "mask":
+                # Add new feature column with mask for whether row is nan or not
+                for i in episode.columns:
+                    episode[i + "_isna"] = episode.loc[:, i].isna()
+
+            elif args.impute_strategy == "ffill":
+                # Fill missing values using forward fill
+                episode = episode.ffill()
+                episode = episode.bfill()  # for remaining null values
+
+            elif args.impute_strategy == "mean":
+                mean_map = get_pop_means(args.subjects_root_path)
+                episode = episode.fillna(mean_map)
+
+            else:
+                raise ValueError(
+                    "impute_strategy must be one of [None, mask, ffill, mean]"
+                )
+
+        if episode.shape[0] < min_events or episode.shape[0] > max_events:
             # if no data for this episode (or less than min or more than max) then continue
             # only keep stays with nb of datapoints within specific range
             filter_by_nb_events += 1
@@ -128,6 +180,11 @@ for subject_dir in tqdm(subject_list, desc="Iterating over subjects"):
             .sort_index(axis=0)
         )
 
+        # TODO: Drop rows with negative "hours"
+
+        # round everything to 1.dp
+        episode = episode.round(1)
+
         # note some stay_id = -1 but this file is just for linking to static variables and can be linked via hadm_id instead
         episodic_data.loc[episodic_data.index == stay_id].to_csv(
             os.path.join(args.subjects_root_path, subject_dir, f"episode{n+1}.csv"),
@@ -139,6 +196,8 @@ for subject_dir in tqdm(subject_list, desc="Iterating over subjects"):
         sorted_indices = [
             i[0] for i in sorted(enumerate(columns_str), key=lambda x: x[1])
         ]
+
+        # TODO: Ensure episodes all have same columns even if missing from episodes
 
         episode = episode[[episode.columns[i] for i in sorted_indices]]
         episode.to_csv(
