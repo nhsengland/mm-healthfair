d96cd6fd4a32e046a21ec006a5cd1eca51f93469
sophmrtn <44570734+sophmrtn@users.noreply.github.com>
2024-05-03 16:56:51 +0100

    Remove unused functions

--- src/data/util.py
+++ src/data/util.py
@@ -1,13 +1,4 @@
-import pandas as pd
 import polars as pl
-
-
-def dataframe_from_csv(path, header=0, chunksize=None, usecols=None):
-    return pd.read_csv(path, header=header, chunksize=chunksize, usecols=usecols)
-
-
-def melt_table(table, id_vars: list = None, value_vars: list = None):
-    return pd.melt(table, id_vars=id_vars, value_vars=value_vars)
 
 
 def get_n_unique_values(df: pl.DataFrame, use_col="subject_id"):
