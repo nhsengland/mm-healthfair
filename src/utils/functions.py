d145c14ae2554ffe1a95edfaaa3714cd7097552a
sophmrtn <44570734+sophmrtn@users.noreply.github.com>
2024-05-29 16:28:41 +0100

    Adds docstrings and remove unused functions

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,6 +1,4 @@
-import os
 import pickle
-from glob import glob
 
 import polars as pl
 
@@ -10,24 +8,50 @@ def impute_from_df(
     impute_from: pl.DataFrame,
     use_col: str = None,
     key_col: str = None,
-):
+) -> pl.DataFrame | pl.LazyFrame:
+    """Imputes values from one dataframe to another.
+
+    Args:
+        impute_to (pl.DataFrame | pl.LazyFrame): Table to impute values in to.
+        impute_from (pl.DataFrame): Table to impute values from.
+        use_col (str, optional): Column to containing values to impute. Defaults to None.
+        key_col (str, optional): Column to use to identify matching rows. Defaults to None.
+
+    Returns:
+        pl.DataFrame | pl.LazyFrame: _description_
+    """
+    # create dictionary mapping values to identifier key
     dict_map = impute_from.select([key_col, use_col]).rows_by_key(
         key=use_col, unique=True
     )
 
+    # create temporary column contaning imputed values
     impute_to = impute_to.with_columns(tmp=pl.col(use_col).replace(dict_map))
+
+    # only use imputed values when missing in original table
     impute_to = impute_to.with_columns(
         pl.when(pl.col(key_col).is_null())
         .then(pl.col("tmp"))
         .otherwise(pl.col(key_col))
         .alias(key_col)
-    ).drop("tmp")
+    ).drop("tmp")  # remove temporary column
 
     return impute_to
 
 
-def get_n_unique_values(df: pl.DataFrame | pl.LazyFrame, use_col="subject_id"):
-    unique_vals = df.select(use_col).unique()
+def get_n_unique_values(
+    table: pl.DataFrame | pl.LazyFrame, use_col: str = "subject_id"
+) -> int:
+    """Compute number of unique values in particular column in table.
+
+    Args:
+        table (pl.DataFrame | pl.LazyFrame): Table.
+        use_col (str, optional): Column to use. Defaults to "subject_id".
+
+    Returns:
+        int: Number of unique values.
+    """
+    unique_vals = table.select(use_col).unique()
     return (
         unique_vals.count().collect(streaming=True).item()
         if type(unique_vals) == pl.LazyFrame
@@ -35,85 +59,58 @@ def get_n_unique_values(df: pl.DataFrame | pl.LazyFrame, use_col="subject_id"):
     )
 
 
-def get_feature_list(subjects_root_path):
-    features = (
-        pl.concat(
-            [
-                pl.scan_csv(f).select(pl.col("label"))
-                for f in glob.glob(os.path.join(subjects_root_path, "*", "events.csv"))
-            ],
-            how="diagonal",
-        )
-        .unique()
-        .collect()
-    )
-    return sorted(features.get_column("label").to_list())
-
-
-def get_pop_means(subjects_root_dir: str, output_dir: str = None) -> dict | None:
-    """
-
-    Uses events data to get population-level mean for features in events.csv
+def scale_numeric_features(
+    table: pl.DataFrame, numeric_cols: list = None, over: str = None
+) -> pl.DataFrame:
+    """Applies min/max scaling to numeric columns and rounds to 1 d.p.
 
     Args:
-        subjects_root_dir (str): Path to subject-level directory.
-        output_dir (str, optional): Path to output directory to save csv. Defaults to None.
+        table (pl.DataFrame): Table.
+        numeric_cols (list, optional): List of columns to apply to. Defaults to None.
+        over (str, optional): Column to group by before computing min/max. Defaults to None.
 
     Returns:
-        dict | None: Save a csv file containing features and mean values to output_dir or return as a dict for mapping.
+        pl.DataFrame: Updated table.
     """
-    events_files = glob(os.path.join(subjects_root_dir, "*", "events.csv"))
-
-    # Use polars to scan all csvs
-    events = pl.concat(
-        [
-            pl.scan_csv(f, null_values=["___"]).select(["value", "label"])
-            for f in events_files
-        ]
-    )
-
-    mean_values = (
-        events.group_by(pl.col("label"))
-        .agg(pl.col("value").mean())
-        .drop_nulls(subset="value")
-        .collect(streaming=True)
-    )
-
-    # Write to disk
-    if output_dir is not None:
-        mean_values.write_csv(os.path.join(output_dir, "mean_values.csv"))
-
-    # Or return mapping as a dictionary
-    return dict(mean_values.iter_rows())
-
-
-def scale_numeric_features(data, numeric_cols=None, over=None):
     # Normalise/scale specified cols using MinMax scaling
     if over is None:
-        scaled = data.select(
+        scaled = table.select(
             (pl.col(numeric_cols) - pl.col(numeric_cols).min())
             / (pl.col(numeric_cols).max() - pl.col(numeric_cols).min())
         )
     else:
         # compute min max of cols over another groupby col e.g., subject_id or label
-        scaled = data.select(
+        scaled = table.select(
             (pl.col(numeric_cols) - pl.col(numeric_cols).min())
             / (pl.col(numeric_cols).max() - pl.col(numeric_cols).min()).over(over)
         )
 
     # ensure all scaled features are floats to 1 d.p
     scaled = scaled.with_columns(pl.all().round(1))
-    return data.select(pl.col("*").exclude(numeric_cols)).hstack(scaled)
+    return table.select(pl.col("*").exclude(numeric_cols)).hstack(scaled)
 
 
-def preview_data(path_to_pkl):
+def preview_data(path_to_pkl: str) -> None:
+    """Prints a single example from data dictionary.
+
+    Args:
+        path_to_pkl (str): Path to .pkl file containing data dictionary.
+    """
     with open(path_to_pkl, "rb") as f:
         data_dict = pickle.load(f)
     example_id = list(data_dict.keys())[-1]
     print(f"Example data:{data_dict[example_id]}")
 
 
-def read_from_txt(filepath):
+def read_from_txt(filepath: str) -> list:
+    """Read from line-seperated txt file.
+
+    Args:
+        filepath (str): Path to text file.
+
+    Returns:
+        list: List containing data.
+    """
     with open(filepath) as f:
         data = [str(line.strip()) for line in f.readlines()]
     return data
--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -1,4 +1,3 @@
-import numpy as np
 import polars as pl
 
 ###############################
@@ -6,24 +5,57 @@ import polars as pl
 ###############################
 
 
-def transform_gender(data):
+def transform_gender(data: pl.DataFrame) -> pl.DataFrame:
+    """Maps gender values to predefined categories.
+
+    Args:
+        data (pl.DataFrame): Data to apply to.
+
+    Returns:
+        pl.DataFrame: Updated data.
+    """
+
     g_map = {"F": 1, "M": 2, "OTHER": 3}
     return data.with_columns(gender=pl.col("gender").replace(g_map, default=0))
 
 
-def transform_marital(data):
+def transform_marital(data: pl.DataFrame) -> pl.DataFrame:
+    """Maps marital status values to predefined categories.
+
+    Args:
+        data (pl.DataFrame): Data to apply to.
+
+    Returns:
+        pl.DataFrame: Updated data.
+    """
     m_map = {"MARRIED": 1, "SINGLE": 2, "WIDOWED": 3, "DIVORCED": 4}
     return data.with_columns(
         marital_status=pl.col("marital_status").replace(m_map, default=0)
     )
 
 
-def transform_insurance(data):
+def transform_insurance(data: pl.DataFrame) -> pl.DataFrame:
+    """Maps insurance status values to predefined categories.
+
+    Args:
+        data (pl.DataFrame): Data to apply to.
+
+    Returns:
+        pl.DataFrame: Updated data.
+    """
     i_map = {"Medicare": 1, "Medicaid": 2, "Other": 3}  # TODO: 0 or nan?
     return data.with_columns(insurance=pl.col("insurance").replace(i_map, default=0))
 
 
-def transform_race(race_series):
+def transform_race(data: pl.DataFrame) -> pl.DataFrame:
+    """Maps race values to predefined categories.
+
+    Args:
+        data (pl.DataFrame): Data to apply to.
+
+    Returns:
+        pl.DataFrame: Updated data.
+    """
     r_map = {
         "ASIAN": 1,
         "BLACK": 2,
@@ -41,29 +73,40 @@ def transform_race(race_series):
         "OTHER": 0,
     }
 
-    race_series = race_series.with_columns(
-        race=pl.col("race").str.replace(" OR ", "/", literal=True)
-    )
-    race_series = race_series.with_columns(
+    # Identifies values with OR
+    data = data.with_columns(race=pl.col("race").str.replace(" OR ", "/", literal=True))
+
+    # Strips first value from e.g., White - European
+    data = data.with_columns(
         pl.col("race")
         .str.split_exact(" - ", n=1)
         .struct.rename_fields(["race_a", "race_b"])
         .alias("race")
     ).unnest("race")
-    race_series = race_series.with_columns(
+
+    # Strips first value from e.g., White / Portuguese
+    data = data.with_columns(
         pl.col("race_a")
         .str.split_exact("/", n=1)
         .struct.rename_fields(["race_c", "race_d"])
         .alias("race")
     ).unnest("race")
 
-    race_series = race_series.with_columns(
-        race=pl.col("race_c").replace(r_map, default=0)
-    ).drop(columns=["race_a", "race_b", "race_c", "race_d"])
-    return race_series
+    data = data.with_columns(race=pl.col("race_c").replace(r_map, default=0)).drop(
+        columns=["race_a", "race_b", "race_c", "race_d"]
+    )
+    return data
 
 
-def encode_categorical_features(stays):
+def encode_categorical_features(stays: pl.DataFrame) -> pl.DataFrame:
+    """Groups and applied one-hot encoding to categorical features.
+
+    Args:
+        stays (pl.DataFrame): Stays data.
+
+    Returns:
+        pl.DataFrame: Transformed stays data.
+    """
     stays = transform_gender(stays)
     stays = transform_race(stays)
     stays = transform_marital(stays)
@@ -80,11 +123,15 @@ def encode_categorical_features(stays):
 ###############################
 
 
-def map_itemids_to_variables(events, var_map):
-    return events.join(var_map, on="itemid")
+def clean_events(events: pl.DataFrame) -> pl.DataFrame:
+    """Maps non-integer values to None and removes outliers.
 
+    Args:
+        events (pl.DataFrame): Events table.
 
-def clean_events(events):
+    Returns:
+        pl.DataFrame: Cleaned events table.
+    """
     # label '__' as null value
     # also converts to 2 d.p. floats
     events = events.with_columns(
@@ -104,7 +151,19 @@ def clean_events(events):
     return events
 
 
-def add_time_elapsed_to_events(events, starttime, remove_charttime=False):
+def add_time_elapsed_to_events(
+    events: pl.DataFrame, starttime: pl.Datetime, remove_charttime: bool = False
+) -> pl.DataFrame:
+    """Adds column 'elapsed' which considers time elapsed since starttime.
+
+    Args:
+        events (pl.DataFrame): Events table.
+        starttime (pl.Datetime): Reference start time.
+        remove_charttime (bool, optional): Whether to remove charttime column. Defaults to False.
+
+    Returns:
+        pl.DataFrame: Updated events table.
+    """
     events = events.with_columns(
         elapsed=((pl.col("charttime") - starttime) / pl.duration(hours=1)).round(1)
     )
@@ -116,7 +175,16 @@ def add_time_elapsed_to_events(events, starttime, remove_charttime=False):
     return events
 
 
-def convert_events_to_timeseries(events):
+def convert_events_to_timeseries(events: pl.DataFrame) -> pl.DataFrame:
+    """Converts long-form events to wide-form time-series.
+
+    Args:
+        events (pl.DataFrame): Long-form events.
+
+    Returns:
+        pl.DataFrame: Wide-form time-series of shape (timestamp, features)
+    """
+
     metadata = (
         events.select(["charttime", "label", "value", "linksto"])
         .sort(by=["charttime", "label", "value"])
@@ -124,39 +192,20 @@ def convert_events_to_timeseries(events):
         .sort(by="charttime")
     )
 
+    # get unique label, values and charttimes
     timeseries = (
         events.select(["charttime", "label", "value"])
         .sort(by=["charttime", "label", "value"])
         .unique(subset=["charttime", "label"], keep="last")
     )
+
+    # pivot into wide-form format
     timeseries = timeseries.pivot(
         index="charttime", columns="label", values="value"
     ).sort(by="charttime")
 
+    # join any metadata remaining
     timeseries = timeseries.join(
         metadata.select(["charttime", "linksto"]), on="charttime", how="inner"
     )
     return timeseries
-
-
-def get_events_in_period(events, stay_id, hadm_id, starttime=None, endtime=None):
-    if starttime is not None and endtime is not None:
-        # also filter so events must fall within start and end time
-        events = events.filter(pl.col("charttime").is_between(starttime, endtime))
-
-    # get events linked to ED stay or hosp admission
-    events = events.filter(
-        (pl.col("stay_id") == stay_id) | (pl.col("hadm_id") == hadm_id)
-    )
-
-    events = events.drop(["stay_id", "hadm_id", "linksto"])
-    return events
-
-
-def get_first_valid_from_timeseries(timeseries, variable):
-    if variable in timeseries:
-        idx = timeseries[variable].notnull()
-        if idx.any():
-            loc = np.where(idx)[0][0]
-            return timeseries[variable].iloc[loc]
-    return np.nan
