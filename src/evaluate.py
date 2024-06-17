d5bc8f1be5d7918e119a54ad3ae268e260a28662
sophmrtn <44570734+sophmrtn@users.noreply.github.com>
2024-06-17 10:42:33 +0100

    Updates fairness evaluation

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,28 +4,39 @@ import time
 
 import matplotlib.pyplot as plt
 import numpy as np
+import polars as pl
 import shap
 import toml
-from datasets import MIMIC4Dataset
+from datasets import CollateTimeSeries, MIMIC4Dataset
 from fairlearn.metrics import (
     MetricFrame,
+    count,
+    demographic_parity_ratio,
+    equalized_odds_difference,
+    equalized_odds_ratio,
     false_negative_rate,
     false_positive_rate,
+    selection_rate,
 )
+from lightning.pytorch import Trainer
+from models import MMModel
 from sklearn.metrics import (
     accuracy_score,
     average_precision_score,
     balanced_accuracy_score,
     roc_auc_score,
 )
+from torch import concat
+from torch.utils.data import DataLoader
 from utils.functions import load_pickle, read_from_txt
+from utils.preprocessing import transform_race
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "data_dir",
         type=str,
-        help="Path to the directory containing processed data (and metadata).",
+        help="Path to the directory containing processed data.",
     )
     parser.add_argument(
         "model_path",
@@ -38,6 +49,11 @@ if __name__ == "__main__":
         type=str,
         default="config.toml",
         help="Path to config toml file containing parameters.",
+    )
+    parser.add_argument(
+        "--metadata",
+        type=str,
+        help="Path to stays.csv containing hadm_id and raw data. Specify if processed data is in different directory to stays.csv.",
     )
     parser.add_argument(
         "--test", default=None, help="List of ids to use for evaluation."
@@ -53,41 +69,64 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     data_path = os.path.join(args.data_dir, "processed_data.pkl")
-    metadata_path = os.path.join(args.data_dir, "metadata.pkl")
+    metadata_path = (
+        os.path.join(args.data_dir, "stays.csv")
+        if args.metadata is None
+        else args.metadata
+    )
 
     config = toml.load(args.config)
     los_threshold = config["model"]["threshold"]
 
+    # if loading from .ckpt (deep learning) set static_only to False else assume static only model (RF)
+    model_type = "fusion" if os.path.splitext(args.model_path)[1] == ".ckpt" else "rf"
+    static_only = True if model_type == "rf" else False
+
     # Get training and validation ids
     test_ids = read_from_txt(args.test) if args.test is not None else None
 
-    print("Loading dataset...")
-
     test_set = MIMIC4Dataset(
+        data_path,
         "test",
         ids=test_ids,
         los_thresh=los_threshold,
-        static_only=True,
+        static_only=static_only,
     )
     test_set.print_label_dist()
 
-    metadata = load_pickle(metadata_path)
+    if model_type == "rf":
+        print("Loading dataset...")
+        x_test = []
+        y_test = []
+        for data in test_set:
+            x, y = data
+            x_test.append(x[0])
+            y_test.append(y[0])
 
-    x_test = []
-    y_test = []
-    for data in test_set:
-        x, y = data
-        x_test.append(x[0])
-        y_test.append(y[0])
+        x_test = np.array(x_test)
+        y_test = np.array(y_test)
 
-    x_test = np.array(x_test)
-    y_test = np.array(y_test)
+        model = load_pickle(args.model_path)
+        print("Evaluating on validation data...")
+        y_hat = model.predict(x_test)
+        prob = model.predict_proba(x_test)[:, 1]
 
-    model = load_pickle(args.model_path)
+    elif model_type == "fusion":
+        test_dataloader = DataLoader(
+            test_set,
+            batch_size=8,
+            collate_fn=CollateTimeSeries(),
+        )
 
-    print("Evaluating on validation data...")
-    y_hat = model.predict(x_test)
-    prob = model.predict_proba(x_test)[:, 1]
+        model = MMModel.load_from_checkpoint(checkpoint_path=args.model_path)
+        print("Evaluating on validation data...")
+
+        trainer = Trainer(accelerator="gpu")
+        output = trainer.predict(model, dataloaders=test_dataloader)
+        default_thresh = 0.5
+        prob = concat([out[0] for out in output])
+        y_hat = np.where(prob > default_thresh, 1, 0)
+        y_test = concat([out[1] for out in output])
 
     acc = accuracy_score(y_test, y_hat)
     bacc = balanced_accuracy_score(y_test, y_hat)
@@ -161,28 +200,59 @@ if __name__ == "__main__":
     if args.fairness:
         #### Fairness evaluation using fairlearn API
 
-        # Get feature for all test_ids
-        protected_features = ["gender_0", "race_0", "insurance_0"]
-        feature_values = test_set.view_features(protected_features)
+        # Get feature for all test_ids from metadata
+        protected_features = ["gender", "race", "insurance", "marital_status"]
 
-        metrics = {
-            "accuracy": accuracy_score,
-            "false positive rate": false_positive_rate,
-            "false negative rate": false_negative_rate,
-        }
+        for pf in protected_features:
+            metadata = (
+                pl.scan_csv(metadata_path)
+                .filter(pl.col("hadm_id").is_in(list(map(int, test_ids))))
+                .select(pf)
+                .collect()
+            )
 
-        metric_frame = MetricFrame(
-            metrics=metrics,
-            y_true=y_test,
-            y_pred=y_hat,
-            sensitive_features=feature_values,
-        )
-        metric_frame.by_group.plot.bar(
-            subplots=True,
-            layout=[3, 1],
-            colormap="Accent",
-            legend=False,
-            figsize=[12, 8],
-            title=f"Fairness evaluation of {[x.upper() for x in protected_features]}",
-        )
+            # group races
+            if pf == "race":
+                metadata = transform_race(metadata)
+                race_groups = {
+                    0: "Unknown/Other",
+                    1: "Asian",
+                    2: "Black",
+                    3: "Hispanic",
+                    4: "White",
+                }
+                metadata = metadata.with_columns(pl.col("race").replace(race_groups))
+
+            metrics = {
+                "accuracy": accuracy_score,
+                "false positive rate": false_positive_rate,
+                "false negative rate": false_negative_rate,
+                "selection rate": selection_rate,
+                "count": count,
+            }
+
+            metric_frame = MetricFrame(
+                metrics=metrics,
+                y_true=y_test,
+                y_pred=y_hat,
+                sensitive_features=metadata,
+            )
+
+            metric_frame.by_group.plot.bar(
+                subplots=True,
+                layout=[3, 2],
+                colormap="Pastel2",
+                legend=False,
+                figsize=[12, 8],
+                title="Fairness evaluation",
+                xlabel=pf,
+            )
+
+            # fairness
+            eor = equalized_odds_ratio(y_test, y_hat, sensitive_features=metadata)
+            eod = equalized_odds_difference(y_test, y_hat, sensitive_features=metadata)
+            dpr = demographic_parity_ratio(y_test, y_hat, sensitive_features=metadata)
+
+            print(f"EOR: {eor},  EOD:{eod}, DPR: {dpr}")
+
         plt.show()
